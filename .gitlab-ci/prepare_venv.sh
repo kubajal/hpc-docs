f184c5bbbf328a13827338ca019243d7713159f6
ggruszczynski <ggruszczynski@gmail.com>
2020-07-09 15:11:27 +0200

    stable v0

--- .gitlab-ci/prepare_venv.sh
+++ .gitlab-ci/prepare_venv.sh
@@ -11,7 +11,7 @@ if lsvirtualenv | grep "$ENV_NAME" >/dev/null
 then
     mkvirtualenv $ENV_NAME
 else 
-    workon $ENV_NAME && echo "venv activated"
+    workon $ENV_NAME && echo "virtualenv $ENV_NAME activated"
 fi
 
 pip install --upgrade -r requirements.txt
