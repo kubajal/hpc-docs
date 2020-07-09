8a791390351ee50a525d5c432c831decd1f2648e
ggruszczynski <ggruszczynski@gmail.com>
2020-07-09 18:45:57 +0200

    echocho

--- .gitlab-ci/prepare_venv.sh
+++ .gitlab-ci/prepare_venv.sh
@@ -6,9 +6,11 @@ export VIRTUALENVWRAPPER_PYTHON=/bin/python3
 export VIRTUALENVWRAPPER_VIRTUALENV=/home/gitlab-runner-nginx/.local/bin/virtualenv
 source ~/.local/bin/virtualenvwrapper.sh
 
-
+echo "checking lsvirtualenv"
+lsvirtualenv
 if lsvirtualenv | grep "$ENV_NAME" >/dev/null
 then
+    echo "mkvirtualenv $ENV_NAME"
     mkvirtualenv $ENV_NAME
 else 
     echo "aaa"
