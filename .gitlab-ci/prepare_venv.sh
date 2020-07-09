90b8892e3dce99172bfa78dcd6465a62596999e5
ggruszczynski <ggruszczynski@gmail.com>
2020-07-09 19:42:37 +0200

    wip2

--- .gitlab-ci/prepare_venv.sh
+++ .gitlab-ci/prepare_venv.sh
@@ -6,17 +6,16 @@ export VIRTUALENVWRAPPER_PYTHON=/bin/python3
 export VIRTUALENVWRAPPER_VIRTUALENV=/home/gitlab-runner-nginx/.local/bin/virtualenv
 source ~/.local/bin/virtualenvwrapper.sh
 
-echo "checking lsvirtualenv"
+echo "listing lsvirtualenv"
 lsvirtualenv
 # if [[ ! $(lsvirtualenv | grep -w "mkdocs-env") ]] ; then echo "not found" ; else echo "found" ; fi
 
 if [[ ! $(lsvirtualenv | grep -w "mkdocs-env") ]] ; then
-    echo "creating $ENV_NAME"
+    echo "creating new enviroment: $ENV_NAME"
     mkvirtualenv $ENV_NAME
 else 
-    echo "found $ENV_NAME"
+    echo "existing enviroment found $ENV_NAME"
     workon $ENV_NAME && echo "virtualenv $ENV_NAME activated"
-    echo "bbb"
 fi
 
 pip install --upgrade -r requirements.txt
