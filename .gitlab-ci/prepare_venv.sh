03762bffc6309ba47b8bc1dc06f9f463f13612f3
ggruszczynski <ggruszczynski@gmail.com>
2020-07-09 14:25:34 +0200

    wip

--- .gitlab-ci/prepare_venv.sh
+++ .gitlab-ci/prepare_venv.sh
@@ -6,7 +6,7 @@ export VIRTUALENVWRAPPER_PYTHON=/bin/python3
 export VIRTUALENVWRAPPER_VIRTUALENV=/home/gitlab-runner-nginx/.local/bin/virtualenv
 source ~/.local/bin/virtualenvwrapper.sh
 
-ENV_NAME="mkdocs-env"
+
 if lsvirtualenv | grep "$ENV_NAME" >/dev/null
 then
     mkvirtualenv $ENV_NAME
